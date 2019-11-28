f087b7e7efe6038fae6245bef3bcca6606e2420c
Adrián Martín <contacto@adrianmr.com>
2019-11-28 16:42:35 +0100

    Organización de carpetas

--- Setup Control/V1/.scripts/.callbacks/FileSaveCallBack.m
+++
@@ -1,9 +0,0 @@
-function [] = FileSaveCallBack(varargin)
-h=gcf;
-[filename, pathname] = uiputfile('*.fig', 'Save the file as');
-    if isnumeric(filename)
-       disp('User pushed cancel. Not saving anything')
-    else
-       savefig(h,fullfile(pathname, filename))
-    end
-end
--- Setup Control/V1/New folder/GPIBManager/lockin_rt.m
+++
@@ -1,9 +0,0 @@
-function [ r,theta ] = lockin_rt( instrument )
-    % Measures R and Theta of lockin
-    value = query(instrument, 'SNAP?3,4');
-    % Convert string to double 2x1 array
-    value = str2num(num2cell2mat(strsplit(value(:))));
-    r=value(1);
-    theta=value(2);
-end
-
