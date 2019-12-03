124be991cba0df40efb5242dc87a7de98f7a7346
JorgeQ <j.quereda@usal.es>
2019-12-03 16:32:38 +0100

    Rampa de voltaje automática + espectros vs gate

---
+++ .scripts/AutomaticRamps/Spectral_V_Ramp.m
@@ -0,0 +1,29 @@
+VRamp=35:-2.5:0;
+DestinationPath='.\Measurements\AutoSave\Test\V=';
+% Find voltage control panel
+PCSFig=findobj('Type','Figure','Name','PCS');
+VCPanel=findobj('Parent',PCSFig,'Title','Source-Meter Control');
+% Find voltage controls
+SMIndex=findobj('Parent',VCPanel,'Tag','SMIndex');
+SMChannel=findobj('Parent',VCPanel,'Tag','SMChannel');
+SMBias=findobj('Parent',VCPanel,'Tag','SMBias');
+SMGo=findobj('Parent',VCPanel,'String','Go');
+GoToV=SMGo.Callback;
+% Find Run Now button
+WLRampFig=findobj('Type','Figure','Name','WL ramp');
+WLRunNowButton=findobj(WLRampFig,'String','Run Now');
+RunNow=WLRunNowButton.Callback;
+
+% Do measurements and save
+tic
+for n=1:length(VRamp)
+
+SMBias.String=num2str(VRamp(n)); % Set next voltage
+GoToV(); RunNow(); %Go to next voltage and measure WL ramp
+% Save
+MeasurementData=WLRampFig.MeasurementData;
+save([DestinationPath num2str(VRamp(n)) '.mat'],'MeasurementData')
+end
+disp('Measurement Ended')
+toc
+
