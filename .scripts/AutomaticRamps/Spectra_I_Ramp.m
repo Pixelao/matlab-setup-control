9b8a8a3919815db62fbe61e66e8042cd24e06d36
Daniel Vaquero Monte <danivaqu@usal.es>
2019-12-03 19:26:15 +0100

    WL_I_Ramp

--- .scripts/AutomaticRamps/Spectra_I_Ramp.m
+++ .scripts/AutomaticRamps/Spectra_I_Ramp.m
@@ -1,4 +1,5 @@
-VRamp=30:-5:0;
+CurrentRamp=150e-9:-5e-9:5e-9;
+VRamp=-50:0.1:50;
 DestinationPath='.\Measurements\AutoSave\Test\V=';
 % Find voltage control panel
 PCSFig=findobj('Type','Figure','Name','PCS');
@@ -8,20 +9,29 @@ SMIndex=findobj('Parent',VCPanel,'Tag','SMIndex');
 SMChannel=findobj('Parent',VCPanel,'Tag','SMChannel');
 SMBias=findobj('Parent',VCPanel,'Tag','SMBias');
 SMGo=findobj('Parent',VCPanel,'String','Go');
+SMRead=findobj('Parent',VCPanel,'String','Read');
+SMCurrent=findobj('Parent',VCPanel,'Tag','Read');
 GoToV=SMGo.Callback;
+ReadI=SMRead.Callback;
 % Find Run Now button
 WLRampFig=findobj('Type','Figure','Name','WL ramp');
 WLRunNowButton=findobj(WLRampFig,'String','Run Now');
 RunNow=WLRunNowButton.Callback;
 
 % Do measurements and save
+tic
 for n=1:length(VRamp)
-
-SMBias.String=num2str(VRamp(n)); % Set next voltage
-GoToV(); RunNow(); %Go to next voltage and measure WL ramp
-% Save
-MeasurementData=WLRampFig.MeasurementData;
-save([DestinationPath num2str(VRamp(n)) '.mat'],'MeasurementData')
+    SMIndex.String=num2str(2);
+    SMBias.String=num2str(VRamp(n)); % Set next voltage
+    GoToV(); 
+    SMIndex.String=num2str(1);
+    ReadI(); %Read source-drain current
+    if SMCurrent>n*0.2e-9 % 
+        RunNow(); %Measure WL ramp
+        % Save
+        MeasurementData=WLRampFig.MeasurementData;
+        save([DestinationPath num2str(VRamp(n)) '.mat'],'MeasurementData')
+    end
 end
 disp('Measurement Ended')
-
+toc
