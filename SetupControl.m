79b1faa5cf46d15e079a625d20d0bb6c5dd57de1
Adrián <xreno@hotmail.es>
2020-05-28 20:57:33 +0200

    Fixed path on GoToPW function

--- SetupControl.m
+++ SetupControl.m
@@ -242,7 +242,7 @@ classdef SetupControl < handle
         end
         function p = GoToPW(obj,PW)
             disp(['Setting',num2str(PW)])
-            [exec_state,output]=system(['NKTcom\NKTcom.exe -p',num2str(PW)]);
+            [exec_state,output]=system(['.resources\NKTcom\NKTcom.exe -p',num2str(PW)]);
             if exec_state==0
                 disp(['Power set to ',num2str(PW),' %'])
                 p=str2num(output(17:end));
