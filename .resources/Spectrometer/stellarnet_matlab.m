529764fd83e261bc4085663b67eac8a76eb73f71
Adrián <xreno@hotmail.es>
2020-05-29 12:53:58 +0200

    small fix

--- .resources/Spectrometer/stellarnet_matlab.m
+++ .resources/Spectrometer/stellarnet_matlab.m
@@ -11,12 +11,12 @@ xtiming = int64(1);
 scansavg = int64(1);
 smoothing = int64(4);
 
-param = py.stellarnet_driver.setparam(spectrometer,inttime,xtiming,scansavg,smoothing);% calling lib to set parameter
-wav = py.stellarnet_driver.array_get_wav(spectrometer);%getting wavelengths
-spectrum = py.stellarnet_driver.array_spectrum(spectrometer);% getting spectrum
+param = py.stellarnet_driver.setparam(spectrometer,inttime,xtiming,scansavg,smoothing); % calling lib to set parameter
+wav = py.stellarnet_driver.array_get_wav(spectrometer); %getting wavelengths
+spectrum = py.stellarnet_driver.array_spectrum(spectrometer); % getting spectrum
 data = double(py.array.array('d',py.numpy.nditer(spectrum))); %d is for double, coverting spectrum to matlab type
 x = double(py.array.array('d',py.numpy.nditer(wav))); %d is for double, coverting wavelengths to matlab type
 
-plot(x,data)% plotting data
+plot(x,data) % plotting data
 xlabel('Wavelength in nm')
 ylabel('Counts')
