4550f41fd29715b075075e0751bf8d728d6841d9
Adrián <xreno@hotmail.es>
2020-06-03 02:53:35 +0200

    Try catch control for instruments

--- SetupControl.m
+++ SetupControl.m
@@ -11,17 +11,27 @@ classdef SetupControl < handle
         end
         function InitComms(obj)
             instrreset;
-                obj.equipment.LI = gpib('ni',0,8); fopen(obj.equipment.LI); %Lock-in SR830
-                %obj.equipment.LI(2) = gpib('ni',0,9); fopen(obj.equipment.LI(2)); %Lock-in SR830
-                obj.equipment.SM = gpib('ni',0,26); fopen(obj.equipment.SM(1)); %Source meter
-                obj.equipment.SM(2) = gpib('ni',0,28); fopen(obj.equipment.SM(2)); %Source meter
-                obj.equipment.ITC503 =gpib('ni',0,13); fopen(obj.equipment.ITC503);...
-                    obj.equipment.ITC503.EOSMode='read&write';...
-                    obj.equipment.ITC503.EOSCharCode='CR';%TemperatureController
-                %obj.equipment.SM(2) = gpib('ni',0,28); fopen(obj.equipment.SM(2)); %Source meter
-                %obj.equipment.EM = gpib('ni',0,13); fopen(obj.equipment.EM(1)); %Electrometer
-                %obj.equipment.EM(2) = gpib('ni',0,14); fopen(obj.equipment.EM(2)); %Electrometer
-                %pyversion C:\Python27\python.exe %pyversion spectrometer
+            try obj.equipment.LI = gpib('ni',0,8); fopen(obj.equipment.LI); disp("LI 1 FOUND") %Lock-in SR830
+            catch; obj.equipment.LI = []; disp("LI 1 ERROR")
+            end
+            try obj.equipment.LI(2) = gpib('ni',0,9); fopen(obj.equipment.LI(2)); disp("LI 2 FOUND") %Lock-in SR830
+            catch; obj.equipment.LI = []; disp("LI 2 ERROR")
+            end
+            try obj.equipment.SM = gpib('ni',0,26); fopen(obj.equipment.SM(1)); disp("SM 1 FOUND") %Source meter
+            catch; obj.equipment.SM = []; disp("SM 1 ERROR")
+            end
+            try obj.equipment.SM(2) = gpib('ni',0,28); fopen(obj.equipment.SM(2)); disp("SM 2 FOUND") %Source meter
+            catch; obj.equipment.SM = []; disp("SM 2 ERROR")
+            end
+            try obj.equipment.EM = gpib('ni',0,13); fopen(obj.equipment.EM(1)); disp("EM 1 FOUND") %Electrometer
+            catch; obj.equipment.EM = []; disp("EM 1 ERROR")
+            end
+            try obj.equipment.EM(2) = gpib('ni',0,14); fopen(obj.equipment.EM(2)); disp("EM 2 FOUND") %Electrometer
+            catch; obj.equipment.EM = []; disp("EM 2 ERROR")
+            end
+            try obj.equipment.ITC503 = gpib('ni',0,13); fopen(obj.equipment.ITC503);obj.equipment.ITC503.EOSMode='read&write';obj.equipment.ITC503.EOSCharCode='CR'; disp("ITC503 1 FOUND") %TemperatureController
+            catch; obj.equipment.ITC503 = []; disp("ITC503 1 ERROR")
+            end
         end
         function Name = IDN(obj,instr,ind)
             switch instr
