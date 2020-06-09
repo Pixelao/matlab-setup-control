b81c50408fe238e6bd8d43cd33de013f380060b7
PCLAB <danivaqu@usal.es>
2020-06-09 11:27:10 +0200

    Spectrometrer change and WL spectrometer to WL Ramp

--- SetupControl.m
+++ SetupControl.m
@@ -6,13 +6,14 @@ classdef SetupControl < handle
     methods
         %Init Functions
         function obj = SetupControl
-            obj.equipment.LI=[]; %Lock-Inss
+            obj.equipment.LI=[]; %Lock-Ins
             obj.equipment.SM=[]; %Source Meters
             obj.equipment.EM=[]; %Electrometers
             obj.equipment.ITC503=[];%TemperatureController
         end
         function InitComms(obj)
             instrreset;
+            %pyversion C:\Python27\python.exe
             try obj.equipment.LI = gpib('ni',0,8); fopen(obj.equipment.LI); disp("LI 1 FOUND") %Lock-in SR830
             catch; obj.equipment.LI = []; disp("LI 1 ERROR")
             end
@@ -288,19 +289,13 @@ classdef SetupControl < handle
             inttime =  int64(100);
             xtiming = int64(1);
             scansavg = int64(1);
-            smoothing = int64(4);
+            smoothing = int64(2);
             param = py.stellarnet_driver.setparam(spectrometer,inttime,xtiming,scansavg,smoothing); % calling lib to set parameter
             wav = py.stellarnet_driver.array_get_wav(spectrometer); %getting wavelengths
             spectrum = py.stellarnet_driver.array_spectrum(spectrometer); % getting spectrum
             data = double(py.array.array('d',py.numpy.nditer(spectrum))); %d is for double, coverting spectrum to matlab type
             x = double(py.array.array('d',py.numpy.nditer(wav))); %d is for double, coverting wavelengths to matlab type
-            max=0;
-            for i=1:length(data)
-                if data(i)>max
-                    max=data(i);
-                    maxWL=x(i);
-                end
-            end
+            maxWL=x(data==max(data));
         end
         
     end
