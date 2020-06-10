2d3cd32e480bc97e96f54a98295959ee8f58a8d1
Adrián <xreno@hotmail.es>
2020-06-10 23:57:02 +0200

    Inst ITC503 functions

--- instruments/ITC503.m
+++ instruments/ITC503.m
@@ -4,38 +4,34 @@ classdef ITC503 < handle
     end
     methods
 
-        function obj = ITC503
-            obj.equipment.ITC503=[]; % ITC503 Obj
-        end
-
-        function ITC503_Init(obj,gpibaddress)   % Initialize instrument
-            obj.equipment.ITC503 = gpib('ni',0,gpibaddress);  % ITC503
-            fopen(obj.equipment.ITC503);                      % ITC503
-            obj.equipment.ITC503.EOSMode='read&write';        % ITC503 EOSmode
-            obj.equipment.ITC503.EOSCharCode='CR';            % ITC503 CharCode
-        end
-
-        function queryITC(obj,command) % ITC503 query com protocol
+         %ITC503 Functions
+         function x=queryITC(obj,command)
             clrdevice(obj.equipment.ITC503)
-            query(obj.equipment.ITC503,command)
+            x=query(obj.equipment.ITC503,command);
         end
-
-        function T = ITC503_ReadT(obj) % ITC503 Read temperature
-            T=extractAfter(queryITC(obj.equipment.ITC503,'R1'),1);
+        function T = ITC503_ReadT(obj)
+            queryITC(obj,'C3');
+            x=queryITC(obj,'R1');
+            T=str2double(extractAfter(x,1));% read temperature
         end
-
-        function stabilizationT = ITC503_SetT(obj,SetT,Tol,Time) % ITC503 Stabilization to set temperature
-            queryITC(obj.equipment.ITC503,'C3'); % Remote Mode
-            queryITC(obj.equipment.ITC503,['T' num2str(SetT)]); % Set temperature
-            queryITC(obj.equipment.ITC503,'A1');
-            queryITC(obj.equipment.ITC503,'L1'); % Auto-PID
-            Setpoint=str2double(extractAfter(queryITC(obj.equipment.ITC503,'R0'),1));
+        function St = ITC503_ReadSetT(obj)
+            queryITC(obj,'C3');
+            x=queryITC(obj,'R0');
+            St=str2double(extractAfter(x,1));% read set temperature
+        end
+        function stabilizationT = ITC503_SetT(obj,SetT,Tol,Time)
+            queryITC(obj,'C3');%Remote Mode
+            queryITC(obj,['T' num2str(SetT)]); %set temperature
+            queryITC(obj,'A1');
+            queryITC(obj,'L1');%Auto-PID
+            Setpoint=str2double(extractAfter(queryITC(obj,'R0'),1));
             check=0;
             while check<Time
-                T=str2double(extractAfter(queryITC(obj.equipment.ITC503,'R1'),1));% read temperature
+                T=str2double(extractAfter(queryITC(obj,'R1'),1))% read temperature
+                stabilization=0;
                 if Setpoint<T+Tol && Setpoint>T-Tol
                     check=check+1;
-                else 
+                else
                     check=0;
                 end
                 pause(1)
