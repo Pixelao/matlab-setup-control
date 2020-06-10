670ea67b77687b0e2aa635086b909efed8ffc35c
Adrián <xreno@hotmail.es>
2020-06-11 00:05:00 +0200

    Ints MS257 Moved

--- instruments/MS257.m
+++ instruments/MS257.m
@@ -1,15 +1,44 @@
-function r = MS257_SetWL(obj,WL) % MS257 set Wavelenght
-    disp(['Moving to ',num2str(WL)])
-    [exec_state,output]=system(['.resources\MS257com\MS257com.exe -m ',num2str(WL)]);
-    if exec_state==0
-        disp(['Laser set to ',num2str(WL),' nm'])
-        r=str2num(output(17:end));
-    else
-        r=[];
-        error(output)
+classdef MS257 < handle
+    properties
+        equipment
     end
-end
+    methods
+    
+        %MS257 Functions
+        function r = MS257move (~,WL)
+            [exec_state,output]=system(['.resources\MS257com\MS257com.exe -m ',num2str(WL)]);
+            if exec_state==0
+                disp(['MS257 Moving wl to ',num2str(WL),' nm'])
+                r=str2double(output(17:end));
+            else
+                r=[];
+                error(output)
+            end
+        end
+        function MS257scan (~,startwl,endwl)
+            [exec_state,output]=system(['.resources\MS257com\MS257com.exe -s ',num2str(startwl),' ',num2str(endwl)]);
+            if exec_state==0
+                disp(['MS257 Starting scan from ',num2str(startwl),' to ',num2str(endwl),'.'])
+            else
+                error(output)
+            end
+        end
+        function MS257abort (~)
+            [exec_state,output]=system('.resources\MS257com\MS257com.exe -a');
+            if exec_state==0
+                disp('MS257 Aborting scan.')
+            else
+                error(output)
+            end
+        end
+        function MS257status (~)
+            [exec_state,output]=system('.resources\MS257com\MS257com.exe -st');
+            if exec_state==0
+                disp('MS257 Status.')
+            else
+                error(output)
+            end
+        end
 
-function r = MS257_ReadWL(obj) % MS257 read Wavelenght
-    %TODO
+    end
 end
