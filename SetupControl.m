db42f0a87cee722ba2a968469e548ed821058a40
JorgeQ <j.quereda@usal.es>
2020-06-10 10:11:07 +0200

    Small debugging of try-catch instrument initialization

--- SetupControl.m
+++ SetupControl.m
@@ -18,19 +18,19 @@ classdef SetupControl < handle
             catch; obj.equipment.LI = []; disp("LI 1 ERROR")
             end
             try obj.equipment.LI(2) = gpib('ni',0,9); fopen(obj.equipment.LI(2)); disp("LI 2 FOUND") %Lock-in SR830
-            catch; obj.equipment.LI = []; disp("LI 2 ERROR")
+            catch; obj.equipment.LI (2) = []; disp("LI 2 ERROR")
             end
             try obj.equipment.SM = gpib('ni',0,26); fopen(obj.equipment.SM(1)); disp("SM 1 FOUND") %Source meter
             catch; obj.equipment.SM = []; disp("SM 1 ERROR")
             end
             try obj.equipment.SM(2) = gpib('ni',0,28); fopen(obj.equipment.SM(2)); disp("SM 2 FOUND") %Source meter
-            catch; obj.equipment.SM = []; disp("SM 2 ERROR")
+            catch; obj.equipment.SM (2) = []; disp("SM 2 ERROR")
             end
             try obj.equipment.EM = gpib('ni',0,18); fopen(obj.equipment.EM(1)); disp("EM 1 FOUND") %Electrometer
             catch; obj.equipment.EM = []; disp("EM 1 ERROR")
             end
             try obj.equipment.EM(2) = gpib('ni',0,14); fopen(obj.equipment.EM(2)); disp("EM 2 FOUND") %Electrometer
-            catch; obj.equipment.EM = []; disp("EM 2 ERROR")
+            catch; obj.equipment.EM (2) = []; disp("EM 2 ERROR")
             end
             try obj.equipment.ITC503 = gpib('ni',0,13); fopen(obj.equipment.ITC503);obj.equipment.ITC503.EOSMode='read&write';obj.equipment.ITC503.EOSCharCode='CR'; disp("ITC503 1 FOUND") %TemperatureController
             catch; obj.equipment.ITC503 = []; disp("ITC503 1 ERROR")
