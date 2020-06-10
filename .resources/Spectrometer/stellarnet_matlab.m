f4b4a8e41d1febc8f6a2e03b99af241de0aaa16d
Adrián <xreno@hotmail.es>
2020-06-10 03:44:40 +0200

    Weebhook commit test

--- .resources/Spectrometer/stellarnet_matlab.m
+++ .resources/Spectrometer/stellarnet_matlab.m
@@ -1,7 +1,7 @@
 % Use the command below with correct python exe path to load the python version first time only
 %pyversion C:\Python27\python.exe
 spectrometer = py.stellarnet_driver.array_get_spec(0);% calling spectrometer device id
-
+%test
 % setting parameters
 inttime =  int64(100);
 xtiming = int64(1);
