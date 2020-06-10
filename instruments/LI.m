db05f432f16340543af9698368a761179210119c
Adrián <xreno@hotmail.es>
2020-06-11 00:04:36 +0200

    Inst LI functions

--- instruments/LI.m
+++ instruments/LI.m
@@ -9,17 +9,19 @@ classdef LI < handle
             fopen(LI);                      % Lock-in SR830
         end
 
-        function [xr1,yt2] = LI_Read(obj,mode) % Lock-In Read
+        %LI Functions
+        function [xr1,yt2] = LI_Read(obj,mode)
             xr1 = [];
             yt2 = [];
-            if ~(strcmp(mode,'XY') || strcmp(mode,'RT') || strcmp(mode,'CH'))
-                error('GPIBManager.m: Mode should be either "XY", "RT" or "CH".')
+            % Mode should be either xy or rt
+            if ~(strcmp(mode,'xy') || strcmp(mode,'rt') || strcmp(mode,'CH'))
+                error('GPIBManager.m: Mode should be either "xy" or "rt".')
                 return
             end
             for h = obj.equipment.LI
-                if strcmp(mode,'XY')
+                if strcmp(mode,'xy')
                     value = query(h, 'SNAP?1,2');
-                elseif strcmp(mode,'RT')
+                elseif strcmp(mode,'rt')
                     value = query(h, 'SNAP?3,4');
                 elseif strcmp(mode,'CH')
                     value = query(h, 'SNAP?10,11');
@@ -30,6 +32,22 @@ classdef LI < handle
                 xr1(end+1) = value(1);
                 yt2(end+1) = value(2);
             end
+        end
+        function [FREQ] = LI_FreqRead(obj)
+            FREQ=[];
+            for h = obj.equipment.LI
+                value=query(h, 'FREQ?');
+                % Convert string to double 2x1 array
+                value = str2num(value);
+                FREQ(end+1) = value(1);
+            end
+        end
+        function [] = LI_FreqSet(obj,ind,FREQ)
+            h = obj.equipment.LI(ind)
+            message=strcat('FREQ ',num2str(FREQ));
+            fprintf(h, message);
+            % Convert string to double 2x1 array
+            
         end
     end
 end
