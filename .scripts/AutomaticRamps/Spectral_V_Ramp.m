2495d4df42f139a5fb6843a9927510392e3e866e
Daniel Vaquero <danivaqu@usal.es>
2020-06-23 19:53:30 +0200

    Change to measure async spectrum as a function of the gate

--- .scripts/AutomaticRamps/Spectral_V_Ramp.m
+++ .scripts/AutomaticRamps/Spectral_V_Ramp.m
@@ -1,6 +1,6 @@
-VRamp=41:1:45;%select VoltageRamp
-PW=40; %select power off the laser
-DestinationPath='.\Measurements\AutoSave\2020-03-03\V=';%select folder to save
+VRamp=-5:1:40;%select VoltageRamp
+PW=50; %select power off the laser
+DestinationPath='C:\Users\Usuario\Desktop\Medidas\Async\GateSpectra160K\V=';%select folder to save
 % Find voltage control panel
 PCSFig=findobj('Type','Figure','Name','PCS');
 VCPanel=findobj('Parent',PCSFig,'Title','Source-Meter Control');
@@ -17,25 +17,23 @@ ReadI=SMRead.Callback;
 % Find Run Now button
 WLRampFig=findobj('Type','Figure','Name','WL ramp');
 WLRunNowButton=findobj(WLRampFig,'String','Run Now');
+WLRunAsyncButton=findobj(WLRampFig,'String','Run Async');
 RunNow=WLRunNowButton.Callback;
-%move MS257
-system(['C:\Users\Usuario\matlab-setup-control\.resources\NKTcom\NKTcom.exe -off'])
+RunAsync=WLRunAsyncButton.Callback;
 % Do measurements and save
 j=1;
 for n=1:length(VRamp)
     SMBias.String=num2str(VRamp(n)); % Set next voltage
     GoToV();
     pause(0.2)
-        system(['C:\Users\Usuario\matlab-setup-control\.resources\NKTcom\NKTcom.exe -on']) %laser on
         pause(1)
-        system(['C:\Users\Usuario\matlab-setup-control\.resources\NKTcom\NKTcom.exe -p',' ',num2str(PW)]) %laser on
         tic
-        RunNow(); %Measure WL ramp
-        system('C:\Users\Usuario\matlab-setup-control\.resources\NKTcom\NKTcom.exe -off') %laser off
+        RunAsync(); %Measure WL ramp
         % Save
         MeasurementData=WLRampFig.MeasurementData;
         MeasurementData.time(j)=toc;
         save([DestinationPath num2str(VRamp(n)) '.mat'],'MeasurementData')
-        system('C:\Users\Usuario\matlab-setup-control\.resources\MS257com\MS257com.exe -m 800')
 end
+SMBias.String=num2str(0); % Finish and set 0 V
+GoToV();
 disp('Measurement Ended')
