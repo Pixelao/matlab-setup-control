cced8ee11b58261070711086007afc10cc524375
Daniel Vaquero Monte <danivaqu@usal.es>
2020-10-13 16:08:13 +0200

    IV at different powers script

---
+++ .scripts/AutomaticRamps/IV_PW_Ramp.m
@@ -0,0 +1,39 @@
+PWRamp=0:2:100;
+WL=639.5;
+DestinationPath='C:\Users\Usuario\Desktop\Medidas\Gateramp\ExcitonAWL\PW=';%select folder to save
+% Find voltage control panel
+PCSFig=findobj('Type','Figure','Name','PCS');
+VCPanel=findobj('Parent',PCSFig,'Title','Source-Meter Control');
+WLPanel=findobj('Parent',PCSFig,'Title','Wavelength');
+% Find voltage controls
+Channel=[1 2];
+SMIndex=findobj('Parent',VCPanel,'Tag','SMIndex');
+SMChannel=findobj('Parent',VCPanel,'Tag','SMChannel');
+SMBias=findobj('Parent',VCPanel,'Tag','SMBias');
+SMGo=findobj('Parent',VCPanel,'String','Go');
+SMRead=findobj('Parent',VCPanel,'String','Read');
+SMCurrent=findobj('Parent',VCPanel,'Tag','SMCurrent');
+GoToV=SMGo.Callback;
+ReadI=SMRead.Callback;
+% Find Run Now button
+IVRampFig=findobj('Type','Figure','Name','WL ramp');
+IVRunNowButton=findobj(WLRampFig,'String','Run Now');
+RunNow=IVRunNowButton.Callback;
+% Do measurements and save
+for n=1:length(PWRamp)
+    PCSFig.Control.LAgo(PWRamp(n));
+    PCSFig.Control.MS257move(WL);
+    pause(1)
+    PCS.Fig.Control.LAon;
+    tic
+    RunNow(); %Measure IV Ramp
+    % Save
+    MeasurementData=IVRampFig.MeasurementData;
+    MeasurementData.time=toc;
+    PCSFig.Control.LAoff
+    save([DestinationPath num2str(PWRamp(n)) '.mat'],'MeasurementData')
+    pause(5)
+end
+SMBias.String=num2str(0); % Finish and set 0 V
+GoToV();
+disp('Measurement Ended')
